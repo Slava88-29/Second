f1917f22a6b15df4e568e1bdad0ea442fcbc0d89
Slava netologia <s-29@mail.ru>
2024-04-06 15:30:16 +0300

    File

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 0;
+        int account = 100;
+        int fill = 1100;
+        if (fill>1000){
+            bonus= fill / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        account = fill+bonus;
+        System.out.printf("Начислено " + bonus + " бонусов ");
+        System.out.printf("Начислено " + account + " рублей ");
+    }
+}
